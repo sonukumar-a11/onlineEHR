e4bfbff4edd04b4a2ceaca16f974ccaa15616f9a
ak13984 <ak13984@yahoo.com>
2022-01-11 18:23:41 +0530

    doctor model updated

--- backend/models.py
+++ backend/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.core.validators import MinLengthValidator
+import uuid
 
 
 # Create your models here.
 class DoctorDetails(models.Model):
-    doctor_id = models.IntegerField()
+    doctor_id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True)
     doctor_name = models.TextField()
     doctor_email = models.TextField()
     doctor_password = models.CharField(max_length=10, validators=[MinLengthValidator(5)])
