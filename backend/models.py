5595f085b9005cae573d7fa0734cba916f5bef3f
ak13984 <ak13984@yahoo.com>
2022-01-11 21:26:57 +0530

    updating patient details

--- backend/models.py
+++ backend/models.py
@@ -27,4 +27,4 @@ class PatientDetails(models.Model):
     address = models.CharField(max_length=255)
     dob = models.DateTimeField()
     phone_number = models.CharField(max_length=10, null=False, blank=False, unique=False)
-    Doctor = models.ForeignKey(DoctorDetails, on_delete=models.PROTECT)
+    doctor = models.ManyToManyField(DoctorDetails, on_delete=models.PROTECT, blank=True)
