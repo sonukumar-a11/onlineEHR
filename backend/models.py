ab3d2b9a84e712dcce487610ebca63f72beafac4
ak13984 <ak13984@yahoo.com>
2022-01-11 18:03:32 +0530

    doctor model created

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+
 
 # Create your models here.
+class DoctorDetails(models.Model):
+    doctor_id = models.IntegerField()
+    doctor_name = models.TextField()
+    doctor_email = models.TextField()
+    doctor_password = models.CharField(max_length=10, validators=[MinLengthValidator(5)])
+
