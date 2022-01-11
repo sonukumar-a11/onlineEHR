e851972be8c5f5be741b28263af77a8fa18400d8
sonukumar-a11 <sharmasonu04359491@gmail.com>
2022-01-11 18:07:09 +0530

    Upsert new models

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
 # Create your models here.
+
+#
+# class PatientDetails(models.Model):
+#     name = models.CharField(max_length=30)
+#     email_id = models.EmailField()
+#     gender =
+#     address =
+#     dob =
+#     phone_number =
