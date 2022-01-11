e4f93e86fb15c9c077fe3417e8ef2be1528dfb0e
sonukumar-a11 <sharmasonu04359491@gmail.com>
2022-01-11 18:49:42 +0530

    Update patient in models

--- backend/models.py
+++ backend/models.py
@@ -1,11 +1,31 @@
 from django.db import models
-# Create your models here.
+from django.core.validators import MinLengthValidator
 
-#
-# class PatientDetails(models.Model):
-#     name = models.CharField(max_length=30)
-#     email_id = models.EmailField()
-#     gender =
-#     address =
-#     dob =
-#     phone_number =
+from uuid import uuid4
+
+
+# Create your models here.
+class DoctorDetails(models.Model):
+    doctor_id = models.IntegerField()
+    doctor_name = models.TextField()
+    doctor_email = models.TextField()
+    doctor_password = models.CharField(max_length=10, validators=[MinLengthValidator(5)])
+
+
+class PatientDetails(models.Model):
+    GENDER_MALE = "M"
+    GENDER_FEMALE = "F"
+    GENDER_TRANS = "T"
+    GENDER_CHOICE = [
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_TRANS, "Trans")
+    ]
+    id = models.UUIDField(primary_key=True, default=uuid4)
+    name = models.CharField(max_length=30)
+    email_id = models.EmailField(unique=True)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICE, default=GENDER_MALE)
+    address = models.CharField(max_length=255)
+    dob = models.DateTimeField()
+    phone_number = models.CharField(max_length=10, null=False, blank=False, unique=False)
+    Doctor = models.ForeignKey(DoctorDetails, on_delete=models.PROTECT)
