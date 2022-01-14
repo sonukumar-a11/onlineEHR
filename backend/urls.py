e8093d21467aed827140fcfb9d8821016bc69790
dnirmal1 <aishpatil19999@gmail.com>
2022-01-14 16:30:26 +0800

    dose_cretaed

--- backend/urls.py
+++ backend/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('api/doctors/<str:doctorid>/me',views.DoctorViewSet.as_view({"get":"me"})),
-    path('api/doctors/<str:doctorid>/addpatient',views.PatientViewSet.as_view({"post":"addpatient"}))
+    path('api/doctors/<str:doctorid>/addpatient',views.PatientViewSet.as_view({"post":"addpatient"})),
+    path('api/doctors/<str:patientid>/meds',views.MedicationViewSet.as_view({"post":"addpatient"})),
+    path('api/patients/<int:medid>/dose',views.DosageViewSet.as_view({"get":"dose","post":"adddose"}))
 ]
