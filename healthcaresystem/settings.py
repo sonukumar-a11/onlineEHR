a97fa395e4003f5a02667ed9e6490dd5ad912bc4
sonukumar-a11 <sharmasonu04359491@gmail.com>
2022-01-17 17:46:34 +0530

    Backend With auth as well as Email conf

--- healthcaresystem/settings.py
+++ healthcaresystem/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 from datetime import timedelta
 from django.conf import settings
@@ -171,8 +171,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST_USER = 'serviceonlineehr@gmail.com'
-EMAIL_HOST_PASSWORD = 'Nexa9491@'
+EMAIL_HOST_PASSWORD = 'kitelredgfkuynsv'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-
 AUTH_USER_MODEL = 'accounts.User'
